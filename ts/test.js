025aa9c975fa9f765b1bb4984b145da2107a43bb
Neal <1107554021@qq.com>
2020-01-12 22:39:12 +0800

    ts

--- ts/test.js
+++ ts/test.js
@@ -1,50 +1,38 @@
-var message = "Hello world";
-console.log(message);
-var Person = /** @class */ (function () {
-    function Person() {
-    }
-    Person.prototype.name = function () {
-        console.log('Neal');
-    };
-    return Person;
-}());
-var obj = new Person();
-obj.name();
-// 元祖
+// 基础类型
+var isDone = false;
+var decLiteral = 6;
+var hexLiteral = 0xf00d;
+var binaryLiteral = 10;
+var octalLiteral = 484;
+var nameEn = "bob";
+var str = "123456" + isDone;
+var list = [1, 2, 3];
+var list1 = [1, 2, 3, 4];
 var x;
-x = ['Neal', 1];
-// 枚举
+x = ['hello', 10];
+// x = [10, 'hello'];
 var Color;
 (function (Color) {
     Color[Color["Red"] = 0] = "Red";
     Color[Color["Green"] = 1] = "Green";
     Color[Color["Blue"] = 2] = "Blue";
 })(Color || (Color = {}));
-;
-var c = Color.Blue;
-console.log(c, "Color is value.");
-// void 用于表示方法返回的类型，表示该方法没有返回值
-function hello() {
-    console.log(0, 'void: function return void.');
+var c = Color.Green;
+var notSure = 4;
+notSure = 'maybe a string insted';
+notSure = false;
+var list3 = [1, true, "free"];
+function warnUser() {
+    console.log('This is my Warning message');
 }
-hello();
-// null 表示对象值缺失。
-// undefined 用于初始化变量为一个未定义的值
-// never 是其它类型（包括null和undefined）的子类型，代表不会出现的值。
-// any 类型
-/**
- * `1. 变量的值会动态改变时，比如来自用户的输入，任意类型可以让这些变量跳过编译阶段的类型检车`
- * `2. 改写现有代码时，任意值允许在编译时可选择地包含或移除类型检查`
- * `3. 定义存储各种类型数据的数组时`
- */
-// eg: 1
-var xAny = 4;
-xAny = 'I am who i am';
-xAny = false;
-// eg: 2
-var y = 4;
-y.isItExists();
-y.toFixed();
-// eg: 3
-var arrayList = [1, false, 'fine'];
-arrayList[1] = 100;
+var unusable = undefined;
+var unusable2 = null;
+var someValue = "this is a string";
+var strLength = someValue.length; // (someValue as string).length;
+for (var i = 0; i < 10; i++) {
+    (function (i) {
+        setTimeout(function () {
+            console.log(i);
+        }, 100 * i);
+    })(i);
+}
--- ts/test.ts
+++ ts/test.ts
@@ -1,55 +1,43 @@
-var message: string = "Hello world";
-console.log(message);
+// 基础类型
+let isDone: boolean = false;
 
-class Person {
-    name(): void {
-       console.log('Neal');
-    }
-}
+let decLiteral: number = 6;
+let hexLiteral: number = 0xf00d;
+let binaryLiteral: number = 0b1010;
+let octalLiteral: number = 0o744;
 
-var obj = new Person();
-obj.name();
+let nameEn: string = "bob";
+let str: string = `123456${isDone}`;
 
-// 元祖
+let list: number[] = [1, 2, 3];
+let list1: Array<number> = [1, 2, 3, 4];
 let x: [string, number];
-x = ['Neal', 1];
+x = ['hello', 10];
+// x = [10, 'hello'];
 
-// 枚举
-enum Color {Red, Green, Blue};
+enum Color {Red, Green, Blue}
+let c: Color = Color.Green;
 
-let c: Color = Color.Blue;
-console.log(c, `Color is value.`);
+let notSure: any = 4;
+notSure = 'maybe a string insted';
 
-// void 用于表示方法返回的类型，表示该方法没有返回值
+notSure = false;
+let list3: any[] = [1, true, "free"];
 
-function hello(): void {
-    console.log(0, 'void: function return void.');
+function warnUser(): void {
+    console.log('This is my Warning message');
 }
-hello();
 
-// null 表示对象值缺失。
-// undefined 用于初始化变量为一个未定义的值
-// never 是其它类型（包括null和undefined）的子类型，代表不会出现的值。
+let unusable: void = undefined;
+let unusable2: null = null;
 
-// any 类型
-/**
- * `1. 变量的值会动态改变时，比如来自用户的输入，任意类型可以让这些变量跳过编译阶段的类型检车`
- * `2. 改写现有代码时，任意值允许在编译时可选择地包含或移除类型检查`
- * `3. 定义存储各种类型数据的数组时`
- */
-// eg: 1
- let xAny: any = 4;
- xAny = 'I am who i am';
- xAny = false;
-
-// eg: 2
-let y: any = 4;
-y.isItExists();
-y.toFixed();
-
-// eg: 3
-let arrayList: any[] = [1, false, 'fine'];
-arrayList[1] = 100;
-
-
+let someValue: any = "this is a string";
+let strLength: number = (<string>someValue).length; // (someValue as string).length;
+for (var i = 0; i < 10; i++) {
+    (function(i){
+        setTimeout(function(){
+            console.log(i);
+        }, 100*i);
+    })(i);
+}
 
