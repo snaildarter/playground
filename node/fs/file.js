b51e62ac1c9257ce741f6e48c4855dbf5aa6ad8e
Neal <1107554021@qq.com>
2020-01-21 17:40:30 +0800

    node

--- node/fs/file.js
+++ node/fs/file.js
@@ -1,15 +1,33 @@
-// const fs = require('fs');
+const fs = require('fs');
 const os = require('os');
 const util = require('util');
 const { log } = console;
 
-// fs.readFile('input.txt', (err, data) => {
+let err = err => {
+    if (err) {
+        log(err);
+    } else {
+        log('run ok');
+    }
+}
+
+// fs.readFile('./input.txt', (err, data) =>  {
 //     if (err) {
 //         return log(err);
 //     }
 
 //     log('异步读取：', data.toString())
 // });
+
+fs.writeFile('./tmp.md', `\n# node fs write file ${~~(Math.random() * 100)}`, {'flag': 'a'}, err)
+
+// 复制文件
+// fs.copyFile('./tmp.md', './tmp1.md', err);
+
+// 删除文件
+// fs.unlink('./tmp1.md', err)
+
+
 
 // let data = fs.readFileSync('input.txt');
 // log("同步读取：", data.toString())
@@ -18,5 +36,5 @@ const { log } = console;
 
 // log(os.hostname(), os.type(), os.platform(), os.arch(), os.uptime());
 
-log(util.inspect(os.cpus()));
+// log(util.inspect(os.cpus()));
 
---
+++ node/fs/tmp.md
@@ -0,0 +1,17 @@
+# node fs write file# node fs write file
+# node fs write file
+# node fs write file
+# node fs write file
+# node fs write file
+# node fs write file
+# node fs write file 0.06257440442273432
+# node fs write file 0
+# node fs write file 38
+# node fs write file 72
+# node fs write file 2
+# node fs write file 20
+# node fs write file 39
+# node fs write file 70
+# node fs write file 68
+# node fs write file 0
+# node fs write file 28
