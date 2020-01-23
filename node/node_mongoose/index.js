7b59ce8fd869057d8ede5d92a8e09b4b9dba0fb6
Neal <1107554021@qq.com>
2020-01-23 14:24:28 +0800

    node db

--- node/node_mongoose/index.js
+++ node/node_mongoose/index.js
@@ -1,2 +1,13 @@
-const mongoose = require('mongoose');
-
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+ 
+MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("runoob");
+    var myobj = { name: "菜鸟教程", url: "www.runoob" };
+    dbo.collection("site").insertOne(myobj, function(err, res) {
+        if (err) throw err;
+        console.log("文档插入成功");
+        db.close();
+    });
+});
