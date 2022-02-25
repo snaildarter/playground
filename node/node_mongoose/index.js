4991b5d19f9c96ad7646609db134e7cc3baef53d
Neal <1107554021@qq.com>
2022-02-25 12:30:14 +0800

    update mongoose doc

--- node/node_mongoose/index.js
+++ node/node_mongoose/index.js
@@ -1,17 +1,31 @@
-var MongoClient = require("mongodb").MongoClient;
-var url = "mongodb://localhost:27017/";
+const Mongoose = require("mongoose");
+const url = "mongodb://localhost:27017/runoob";
 
-MongoClient.connect(
-  url,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  function (err, db) {
-    if (err) throw err;
-    var dbo = db.db("runoob");
-    var myobj = { name: "菜鸟教程", url: "www.runoob" };
-    dbo.collection("site").insertOne(myobj, function (err, res) {
-      if (err) throw err;
-      console.log("文档插入成功");
-      db.close();
-    });
+Mongoose.connect(url);
+
+const Cat = Mongoose.model("Cat", { name: String });
+
+const kitty = new Cat({ name: "Ben" });
+
+kitty.save(function (err, data) {
+  if (err) {
+    console.log("error", err);
   }
-);
+
+  console.log("data", data, "success");
+});
+
+// MongoClient.connect(
+//   url,
+//   { useNewUrlParser: true, useUnifiedTopology: true },
+//   function (err, db) {s
+//     if (err) throw err;
+//     var dbo = db.db("runoob");
+//     var myobj = { name: "菜鸟教程", url: "www.runoob" };
+//     dbo.collection("site").insertOne(myobj, function (err, res) {
+//       if (err) throw err;
+//       console.log("文档插入成功");
+//       db.close();
+//     });
+//   }
+// );
